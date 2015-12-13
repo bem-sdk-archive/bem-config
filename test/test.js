de466ce3ff93330657e6cb3436a680daa6dc93a5
Ivan Voischev <voischev.ivan@ya.ru>
2015-12-13 22:11:07 +0300

    fix tests

--- test/test.js
+++ test/test.js
@@ -1,44 +1,57 @@
 var mock = require('mock-fs'),
+    config = require('..'),
     path = require('path'),
     expect = require('chai').expect,
-    config = require('..'),
-    localConfigName = config.getConfigFilename(),
-    globalConfigName = config.getConfigFilename(true),
-    findConfigPath = require.resolve('find-config'),
-    osHomedirPath = require.resolve('os-homedir'),
-    tilde = require(osHomedirPath)(),
-    globalConfigPath = path.resolve(tilde, globalConfigName),
-    localConfigPath = path.resolve(process.cwd(), localConfigName),
-    localDirName = path.dirname(localConfigPath);
-
-    require(findConfigPath);
+    tilde = require('os-homedir')(),
+    clearRequire = require('clear-require');
 
 describe('bem-config tests', function() {
+    var localConfigName,
+        globalConfigName,
+        globalConfigPath,
+        localConfigPath,
+        localDirName,
+        newlocalConfigPath,
+        newlocalDirName;
 
-    afterEach(function () {
+    beforeEach(function() {
+        localConfigName = config.getConfigFilename();
+        localConfigPath = path.resolve(process.cwd(), localConfigName);
+        localDirName = path.dirname(localConfigPath);
+        globalConfigName = config.getConfigFilename(true);
+        globalConfigPath = path.resolve(tilde, globalConfigName);
+        newlocalConfigPath = path.resolve('..', localConfigName);
+        newlocalDirName = path.dirname(path.resolve('..', localConfigName));
+    });
+
+    afterEach(function() {
+        try { clearRequire(globalConfigPath)} catch (e) {};
+        try { clearRequire(localConfigPath)} catch (e) {};
+        try { clearRequire(newlocalConfigPath)} catch (e) {};
+        try { clearRequire(newlocalDirName)} catch (e) {};
         mock.restore();
     });
 
     it('should return local config filename', function() {
-        expect('bemconf.json').eql(config.getConfigFilename());
+        expect(config.getConfigFilename()).eql('bemconf.json');
     });
 
     it('should return global config filename', function() {
-        expect('.bemconf.json').eql(config.getConfigFilename(true));
+        expect(config.getConfigFilename(true)).eql('.bemconf.json');
     });
 
     it('should return global config path', function() {
-        expect(path.resolve(tilde, '.bemconf.json')).eql(config.getGlobalConfigPath());
+        expect(config.getGlobalConfigPath()).eql(path.resolve(tilde, '.bemconf.json'));
     });
 
     it('should return empty configs', function() {
         mock({});
 
-        expect({
+        expect(config()).eql({
             global: {},
             local: {},
             extended: {}
-        }).eql(config());
+        });
     });
 
     it('should return global config', function() {
@@ -46,25 +59,25 @@ describe('bem-config tests', function() {
         scheme[globalConfigPath] = '{ "GLOBAL": "1" }';
         mock(scheme);
 
-        expect({
+        expect(config()).eql({
             global: { GLOBAL: '1' },
             local: {},
             extended: { GLOBAL: '1' }
-        }).eql(config());
+        });
     });
 
     it('should respect local config', function() {
         var scheme = {};
 
-        scheme[localConfigPath] = '{ "GLOBAL": "1" }';
+        scheme[globalConfigPath] = '{ "GLOBAL": "2" }';
         scheme[localConfigPath] = '{ "LOCAL": "1", "root": true }';
 
         mock(scheme);
-        expect({
-            global: { GLOBAL: '1' },
+        expect(config()).eql({
+            global: { GLOBAL: '2' },
             local: { LOCAL: '1', root: localDirName },
-            extended: { GLOBAL: '1', LOCAL: '1', root: localDirName }
-        }).eql(config());
+            extended: { GLOBAL: '2', LOCAL: '1', root: localDirName }
+        });
     });
 
     it('should override config with option argument', function() {
@@ -73,26 +86,23 @@ describe('bem-config tests', function() {
         scheme[localConfigPath] = '{ "LOCAL": "1", "root": true }';
         mock(scheme);
 
-        expect({
+        expect(config({ OPTION: '1'})).eql({
             global: { GLOBAL: '1' },
             local: { LOCAL: '1', root: localDirName },
             extended: { GLOBAL: '1', LOCAL: '1', OPTION: '1', root: localDirName }
-        }).eql(config({ OPTION: '1'}));
+        });
     });
 
-    // TODO: fix moch-fs
     it('should find config a level directory up', function() {
-        var scheme = {},
-            newLocalDirName = path.dirname(path.resolve('..', localConfigName));
+        var scheme = {};
         scheme[globalConfigPath] = '{ "GLOBAL": "1" }';
-        scheme[localConfigPath] = ''; // delete config in cwd
-        scheme[path.resolve('..', localConfigName)] = '{ "LOCAL": "1", "root": true }';
+        scheme[newlocalConfigPath] = '{ "LOCAL": "2", "root": true }';
         mock(scheme);
 
-        expect({
+        expect(config({ OPTION: '1'})).eql({
             global: { GLOBAL: '1' },
-            local: { LOCAL: '1', root: newLocalDirName },
-            extended: { GLOBAL: '1', LOCAL: '1', OPTION: '1', root: newLocalDirName }
-        }).eql(config({ OPTION: '1'}));
+            local: { LOCAL: '2', root: newlocalDirName },
+            extended: { GLOBAL: '1', LOCAL: '2', OPTION: '1', root: newlocalDirName }
+        });
     });
 });
